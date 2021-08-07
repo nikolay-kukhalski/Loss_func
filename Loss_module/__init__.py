2a3f6317c16a65468212f5f1e3861e1ee6852647
Nikolay Kukhalski <62181103+graf-by@users.noreply.github.com>
2021-08-07 14:47:59 +0300

    Add files via upload

---
+++ Loss_module/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ["keras_loss"]
+def name():
+  return "Keras model loss & metrics graf"
+def description():
+  return "Построение графиков обучения моделей в keras"
+def version():
+  return "Version 0.1"
