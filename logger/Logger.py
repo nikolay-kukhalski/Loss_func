f15d0d63141a7e0e7f9cd145487a936990fedc4b
Nikolay Kukhalski <kng_by@mail.ru>
2021-08-11 15:55:46 +0300

    many metrics

--- logger/Logger.py
+++ logger/Logger.py
@@ -6,18 +6,19 @@ class CSVLogger:
         self.file_path = file_path + '.csv'
     
 
-    def log(self, metric_key: str, metric_value: float, step: int):
+    def log(self, metric_key: list, metric_value: list, step: int):
 
-        dict_1 = {'epoch':step + 1, metric_key:metric_value}
-
+        dict_1 = {metric_key[i]: metric_value[i] for i in range(0, len(metric_key), 1)}
+        dict_1['epoch'] = step + 1
+        
         if step==0:
             with open(self.file_path, 'w', newline='') as csvfile:
-                fieldnames = ['epoch', metric_key]
+                fieldnames = ['epoch'] + metric_key
                 writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
                 writer.writeheader()
     
         with open(self.file_path, 'a', newline='') as csvfile:
-            fieldnames = ['epoch', metric_key]
+            fieldnames = ['epoch'] + metric_key
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writerow(dict_1)
-        #pass
+        
