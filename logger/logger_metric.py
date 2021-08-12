b12213b9fdf970731110617b8b4088ed082822fe
Nikolay Kukhalski <kng_by@mail.ru>
2021-08-12 17:18:01 +0300

    add List[]

--- logger/logger_metric.py
+++ logger/logger_metric.py
@@ -1,32 +1,34 @@
 import csv
 
+from typing import List 
+
 class CSVLogger:
     
-    def __init__(self, file_path: str, metric_key: list):
+    def __init__(self, file_path: str, metric_key: List[str]):
         self._file_path = file_path 
         self._metric_key = metric_key
         self._fieldnames = ['epoch'] + self._metric_key
 
-        if(type(self._file_path)!=str):
-            raise Exception('\nParameter "file_path" must be str')
+        if not isinstance(self._file_path, str):
+            raise TypeError('\nParameter "file_path" must be str')
 
         if self._file_path[-4:] !='.csv':
             self._file_path = file_path + '.csv'
 
-        if(type(metric_key)!=list):
-            raise Exception('\nParameter "metric_key" must be list')
+        if not isinstance(metric_key, list):
+            raise TypeError('\nParameter "metric_key" must be list')
                      
         with open(self._file_path, 'w', newline='') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=self._fieldnames)
             writer.writeheader()
 
-    def log(self, metric_value: list, step: int):
+    def log(self, metric_value: List[float], step: int):
 
-        if(type(metric_value)!=list):
-            raise Exception('\n\nParameter "metric_value" must be list')
+        if not isinstance(metric_value, list):
+            raise TypeError('\n\nParameter "metric_value" must be list')
 
         if len(self._metric_key) != len(metric_value):
-                raise Exception('\nThe number of metrics does not correspond to the number of values.\nLogging is not possible')
+                raise ValueError('\nThe number of metrics does not correspond to the number of values.\nLogging is not possible')
         
        
         dict_1 = {self._metric_key[i]: metric_value[i] for i in range(0, len(self._metric_key), 1)}
