fc19481dc555a0c923e176387ecde9e0769cff5a
Nikolay Kukhalski <kng_by@mail.ru>
2021-08-12 13:04:57 +0300

    Added list verification

--- logger/logger_metric.py
+++ logger/logger_metric.py
@@ -1,24 +1,35 @@
 import csv
+import sys
+
 
 class CSVLogger:
     
-    def __init__(self, file_path: str):
+    def __init__(self, file_path: str, metric_key: list):
         self.file_path = file_path + '.csv'
+        self.metric_key = metric_key
     
 
-    def log(self, metric_key: list, metric_value: list, step: int):
-
-        dict_1 = {metric_key[i]: metric_value[i] for i in range(0, len(metric_key), 1)}
-        dict_1['epoch'] = step + 1
+    def log(self, metric_value: list, step: int):
         
-        if step==0:
-            with open(self.file_path, 'w', newline='') as csvfile:
-                fieldnames = ['epoch'] + metric_key
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                writer.writeheader()
+        if len(self.metric_key) != len(metric_value):
+            print('\n')
+            print('The number of metrics does not correspond to the number of values.\nLogging is not possible')
+            print('\n')
+            sys.exit()
+
+        else:
+
+            dict_1 = {self.metric_key[i]: metric_value[i] for i in range(0, len(self.metric_key), 1)}
+            dict_1['epoch'] = step + 1
+        
+            if step==0:
+                with open(self.file_path, 'w', newline='') as csvfile:
+                    fieldnames = ['epoch'] + self.metric_key
+                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                    writer.writeheader()
     
-        with open(self.file_path, 'a', newline='') as csvfile:
-            fieldnames = ['epoch'] + metric_key
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writerow(dict_1)
+            with open(self.file_path, 'a', newline='') as csvfile:
+                fieldnames = ['epoch'] + self.metric_key
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writerow(dict_1)
         
